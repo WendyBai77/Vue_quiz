6bf23da6541fe213c13f023613136f45913f907a
WendyBai77 <candy1992517@gmail.com>
2023-05-09 16:46:00 +0800

    deploy

--- assets/HomeView-393571e3.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r,o as t,c as n}from"./index-d1d118a7.js";const c={data(){return{}}};function s(a,_,p,i,u,f){const e=r("router-view");return t(),n(e)}const d=o(c,[["render",s]]);export{d as default};
